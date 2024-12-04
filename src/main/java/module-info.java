d35d0e21de046272e24ef63f3d6bddc21b4a053d
mckayERP <michael.mckay@mckayerp.com>
2024-12-04 18:17:22 -0500

    Update project files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module org.mckayerp.condor_downloader {
     requires java.management;
     requires dev.failsafe.core;
     requires org.apache.commons.configuration2;
+    requires java.logging;
 
     opens org.mckayerp.condor_downloader to javafx.fxml;
     exports org.mckayerp.condor_downloader;
